23ecfcf1cb0421af66b1d09a875755c37a366cc7
plrnvld <paulrijneveld@gmail.com>
2022-03-06 11:07:21 +0000

    Analyzing blocks

--- Main.hs
+++ Main.hs
@@ -1,16 +1,27 @@
 main = do
-    putStrLn "Hello"
-    putStrLn "World"
-
     let allInputs = [1..9]
         varInput = 1
-        calcRes varInput = calcBlock varInput 0 1 12 4
+        calcBlock1 = calcBlock 1 12 4
+        calcBlock2 = calcBlock 1 11 11
+        calcBlock3 = calcBlock 1 13 5
+        calcBlock4 = calcBlock2
+        calcBlock5 = calcBlock 1 14 14
+        calcBlock6 = calcBlock 26 (-10) 7
+        calcBlock7 = calcBlock2
+        calcBlock8 = calcBlock 26 (-9) 4
+        calcBlock9 = calcBlock 26 (-3) 6
+        calcBlock10 = calcBlock3
+        calcBlock11 = calcBlock 26 (-5) 9
+        calcBlock12 = calcBlock 26 (-10) 12
+        calcBlock13 = calcBlock 26 (-4) 14
+        calcBlock14 = calcBlock 26 (-5) 14
+        calcRes = calcBlock6 1000
         makeResText varInput res = "Outcome for " ++ show varInput ++ " = " ++ show res ++ "\n"
 
-    putStrLn $ concat $ map (\i -> (makeResText i (calcRes i))) allInputs
+    putStrLn $ concatMap (\i -> makeResText i (calcRes i)) allInputs
 
 calcBlock :: Int -> Int -> Int -> Int -> Int -> Int
-calcBlock newVar prevZ step5Div step6Add step16Add =
+calcBlock step5Div step6Add step16Add prevZ newVar =
     let w = newVar
         z = prevZ
         x = z -- Step 2 & 3 combined
@@ -28,7 +39,13 @@ calcBlock newVar prevZ step5Div step6Add step16Add =
         z_3 = z_2 + y_5
         in z_3
 
--- Block 1 really adds 4 to the variable
+-- Block 1 really does: f(var) = var + 4
+-- Block 2 really does: f(var, z) = 26z + var + 11
+-- Block 3 really does: f(var, z) = 26z + var + 5
+-- Block 4 is the same as block 2
+-- Block 5 really does: f(var, z) = 26z + var + 14
+-- Block 6 really does: f(var, z) = var + 7? Not true, bigger numbers give bigger results
+
 
         
         
