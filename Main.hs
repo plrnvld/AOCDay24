fc40b7fe757d2ea308ff611f315bf8c932a912d2
plrnvld <paulrijneveld@gmail.com>
2022-03-09 19:18:54 +0000

    Calculate the first model number

--- Main.hs
+++ Main.hs
@@ -1,5 +1,12 @@
 main = do
-    let allInputs = [1..9]
+    let bigNum = replicate 14 9
+        blocks = [calcBlock1, calcBlock2, calcBlock3, calcBlock4, 
+            calcBlock5, calcBlock6, calcBlock7, calcBlock8, calcBlock9, 
+            calcBlock10, calcBlock11, calcBlock12, calcBlock13, calcBlock14]
+        blockPerDigit = zipWith (\block num -> flip block num) blocks bigNum
+        outcome = foldr (\f v -> f v) 0 blockPerDigit
+
+        allInputs = [1..9]
         varInput = 1
         calcBlock1 = calcBlock 1 12 4
         calcBlock2 = calcBlock 1 11 11
@@ -20,6 +27,8 @@ main = do
         makeResText varInput res = "Outcome for " ++ show varInput ++ " = " ++ show res ++ "\n"
 
     putStrLn $ concatMap (\i -> makeResText i (calcRes i)) allInputs
+    putStrLn $ show bigNum
+    putStrLn $ show outcome
 
 calcBlock :: Int -> Int -> Int -> Int -> Int -> Int
 calcBlock step5Div step6Add step16Add z newVar =
@@ -37,14 +46,18 @@ calcBlockAlt step5Div step6Add step16Add z newVar =
     let x_1 = if z `mod` 26 + step6Add == newVar then 0 else 1 -- Step 7 & 8 combined
         in (25 * (z `div` step5Div) + newVar + step16Add) * x_1 + (z `div` step5Div)
 
+digits :: Integer -> [Int]
+digits = map (read.return) . show
+
+
 -- when step5Div == 1 and step6Add > 9
 -- 26 * z + step16Add + newVar
 
 -- when step5Div == 26 and z `mod` 26 + step6Add = newVar
--- z `div` 26 (step6Add and newVar are not in the result)
+-- z `div` 26                                   (step6Add and newVar are not in the result)
 
 -- when step5Div == 26 and z `mod` 26 + step6Add != newVar
--- 26 * (z `div` 26) + step16Add + newVar (step6Add is not in the result)
+-- 26 * (z `div` 26) + step16Add + newVar       (step6Add is not in the result)
 
 -- Block 1 really does: f(var) = var + 4
 -- Block 2 really does: f(var, z) = 26z + var + 11
